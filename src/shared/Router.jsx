9dca5c4fd900cc6ae33e07ec54de682658c8e5b1
Jo <johj703@nate.com>
2024-08-25 23:54:58 +0900

    Router 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,6 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const Router = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
+          <Route path="/" element={<Contact />} />
+          <Route path="/" element={<Works />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
